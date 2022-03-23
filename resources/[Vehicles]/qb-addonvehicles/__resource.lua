963e5ca3ca89caeb404b8ad3d115d718ca7148e7
SkyHavenRP <skyhavenrp@gmail.com>
2022-03-23 19:37:17 +0000

    cars final

---
+++ resources/[Vehicles]/qb-addonvehicles/__resource.lua
@@ -0,0 +1,191 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+client_script 'client/vehicle_names.lua'
+ 
+files {
+	'vehicles.meta',
+	'carvariations.meta',
+	'carcols.meta',
+	'handling.meta',
+	
+	--20x5m
+	'metas/20x5m/handling.meta',
+	'metas/20x5m/carcols.meta',
+	'metas/20x5m/vehicles.meta',
+	'metas/20x5m/carvariations.meta',
+	'metas/20x5m/x5omcontentunlocks.meta',
+	'metas/20x5m/vehiclelayouts.meta',
+	
+	--rrab
+	'metas/rrab/handling.meta',
+	'metas/rrab/carcols.meta',
+	'metas/rrab/vehicles.meta',
+	'metas/rrab/carvariations.meta',
+	'metas/rrab/autobiocontentunlocks.meta',
+	
+	--mansgt
+	'metas/mansgt/handling.meta',
+	'metas/mansgt/carcols.meta',
+	'metas/mansgt/vehicles.meta',
+	'metas/mansgt/carvariations.meta',
+	
+	--
+	--
+	--
+	--pstmansory
+	'metas/pstmansory/handling.meta',
+	'metas/pstmansory/carcols.meta',
+	'metas/pstmansory/vehicles.meta',
+	'metas/pstmansory/carvariations.meta',
+	
+	-- golf7gti
+	'metas/golf7gti/handling.meta',
+	'metas/golf7gti/carcols.meta',
+	'metas/golf7gti/vehicles.meta',
+	'metas/golf7gti/carvariations.meta',
+
+	-- hurevo
+	'metas/hurevo/handling.meta',
+	'metas/hurevo/vehicles.meta',
+	'metas/hurevo/carvariations.meta',
+
+	-- q8prior
+	'metas/q8prior/handling.meta',
+	'metas/q8prior/carcols.meta',
+	'metas/q8prior/vehicles.meta',
+	'metas/q8prior/carvariations.meta',
+	
+	-- BRABUS850
+	'metas/brabus850/handling.meta',
+	'metas/brabus850/vehicles.meta',
+	'metas/brabus850/carvariations.meta',
+ 
+	-- BMW M5 F90
+	'metas/bmci/handling.meta',
+	'metas/bmci/carcols.meta',
+	'metas/bmci/vehicles.meta',
+	'metas/bmci/carvariations.meta',
+	
+    --rapide
+    'metas/rapide/handling.meta',
+    'metas/rapide/carcols.meta',
+    'metas/rapide/vehicles.meta',
+    'metas/rapide/carvariations.meta',
+	
+	-- MB A45 AMG (a45amg)
+	'metas/a45amg/handling.meta',
+	'metas/a45amg/carcols.meta',
+    'metas/a45amg/vehicles.meta',
+    'metas/a45amg/carvariations.meta',
+	
+    --audirs6tk
+    'metas/audirs6tk/handling.meta',
+    'metas/audirs6tk/carcols.meta',
+    'metas/audirs6tk/vehicles.meta',
+    'metas/audirs6tk/carvariations.meta',
+	
+	--skyline
+    'metas/skyline/handling.meta',
+    'metas/skyline/carcols.meta',
+    'metas/skyline/vehicles.meta',
+    'metas/skyline/carvariations.meta',
+	
+	-- g63
+	'metas/g632019/handling.meta',
+	'metas/g632019/carcols.meta',
+    'metas/g632019/vehicles.meta',
+    'metas/g632019/carvariations.meta',
+	'metas/g632019/caraddoncontentunlocks.meta',
+
+}
+ 
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+
+--20x5m
+data_file 'HANDLING_FILE' 'metas/20x5m/handling.meta'
+data_file 'CARCOLS_FILE' 'metas/20x5m/carcols.meta'
+data_file 'VEHICLE_METADATA_FILE' 'metas/20x5m/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'metas/20x5m/carvariations.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'metas/20x5m/x5omcontentunlocks.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'metas/20x5m/vehiclelayouts.meta'
+
+--rrab
+data_file 'HANDLING_FILE' 'metas/rrab/handling.meta'
+data_file 'CARCOLS_FILE' 'metas/rrab/carcols.meta'
+data_file 'VEHICLE_METADATA_FILE' 'metas/rrab/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'metas/rrab/carvariations.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'metas/rrab/autobiocontentunlocks.meta'
+
+--mansgt
+data_file 'HANDLING_FILE' 'metas/mansgt/handling.meta'
+data_file 'CARCOLS_FILE' 'metas/mansgt/carcols.meta'
+data_file 'VEHICLE_METADATA_FILE' 'metas/mansgt/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'metas/mansgt/carvariations.meta'
+
+--pstmansory
+data_file 'HANDLING_FILE' 'metas/pstmansory/handling.meta'
+data_file 'CARCOLS_FILE' 'metas/pstmansory/carcols.meta'
+data_file 'VEHICLE_METADATA_FILE' 'metas/pstmansory/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'metas/pstmansory/carvariations.meta'
+
+--golf7gti
+data_file 'HANDLING_FILE' 'metas/golf7gti/handling.meta'
+data_file 'CARCOLS_FILE' 'metas/golf7gti/carcols.meta'
+data_file 'VEHICLE_METADATA_FILE' 'metas/golf7gti/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'metas/golf7gti/carvariations.meta'
+
+--hurevo
+data_file 'HANDLING_FILE' 'metas/hurevo/handling.meta'
+data_file 'CARCOLS_FILE' 'metas/hurevo/carcols.meta'
+data_file 'VEHICLE_METADATA_FILE' 'metas/hurevo/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'metas/hurevo/carvariations.meta'
+
+--q8prior
+data_file 'HANDLING_FILE' 'metas/q8prior/handling.meta'
+data_file 'CARCOLS_FILE' 'metas/q8prior/carcols.meta'
+data_file 'VEHICLE_METADATA_FILE' 'metas/q8prior/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'metas/q8prior/carvariations.meta'
+
+--brabus850
+data_file 'HANDLING_FILE' 'metas/brabus850/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'metas/brabus850/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'metas/brabus850/carvariations.meta'
+
+--skyline
+data_file 'HANDLING_FILE' 'metas/skyline/handling.meta'
+data_file 'CARCOLS_FILE' 'metas/skyline/carcols.meta'
+data_file 'VEHICLE_METADATA_FILE' 'metas/skyline/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'metas/skyline/carvariations.meta'
+
+--audirs6tk
+data_file 'HANDLING_FILE' 'metas/audirs6tk/handling.meta'
+data_file 'CARCOLS_FILE' 'metas/audirs6tk/carcols.meta'
+data_file 'VEHICLE_METADATA_FILE' 'metas/audirs6tk/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'metas/audirs6tk/carvariations.meta'
+
+-- MB A45 AMG (a45amg)
+data_file 'HANDLING_FILE' 'metas/a45amg/handling.meta'
+data_file 'CARCOLS_FILE' 'metas/a45amg/carcols.meta'
+data_file 'VEHICLE_METADATA_FILE' 'metas/a45amg/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'metas/a45amg/carvariations.meta'
+
+--rapide
+data_file 'HANDLING_FILE' 'metas/rapide/handling.meta'
+data_file 'CARCOLS_FILE' 'metas/rapide/carcols.meta'
+data_file 'VEHICLE_METADATA_FILE' 'metas/rapide/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'metas/rapide/carvariations.meta'
+
+--BMW M5 F90
+data_file 'HANDLING_FILE' 'metas/bmci/handling.meta'
+data_file 'CARCOLS_FILE' 'metas/bmci/carcols.meta'
+data_file 'VEHICLE_METADATA_FILE' 'metas/bmci/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'metas/bmci/carvariations.meta'
+
+-- g63
+data_file 'HANDLING_FILE' 'metas/g632019/handling.meta'
+data_file 'CARCOLS_FILE' 'metas/g632019/carcols.meta'
+data_file 'VEHICLE_METADATA_FILE' 'metas/g632019/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'metas/g632019/carvariations.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'metas/g632019/caraddoncontentunlocks.meta'
