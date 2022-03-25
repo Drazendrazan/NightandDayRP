e401e80c62639a86ee6588da03448984df6e2178
SkyHavenRP <skyhavenrp@gmail.com>
2022-03-25 13:34:15 +0000

    cars part 1/4

---
+++ resources/[Vehicles]/[new-cars]/ffrp-handling/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'adamant'
+
+games { 'rdr3', 'gta5' }
+
+files {
+    'data/handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/handling.meta'
+client_script "@Badger-Anticheat/acloader.lua"
